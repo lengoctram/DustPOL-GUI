31ccd09af7a3033ea856411452d5d4988e41ab02
lengoctram <91097424+lengoctram@users.noreply.github.com>
2024-08-28 13:49:45 +0200

    Update dash-layout.py

--- dash-layout.py
+++ dash-layout.py
@@ -145,11 +145,7 @@ def plot_figures():
                 results = execute_DustPOL(U_rad, n_gas, f_max, grain_type, grain_shape, amax, amin, rat_theory, Bfield, Ncl,p_plot_option)
                 if p_plot_option == 'Both':
                     w, pext, pem, A_per_Ngas = results
-                    w_new = np.linspace(w[0],w[-1],5000)
-                    ## smooth pext
-                    pext_new=scipy.interpolate.interp1d(w,pext,kind='cubic')(w_new)
-                    ax1.semilogx(w_new * 1e4, pext_new / n_gas, label=f'U={U_rad:.1f} -- n$_{{\\rm H}}$={n_gas:.1e} -- f$_{{\\rm max}}$={f_max:.2f}')
-                    ax1.semilogx(w * 1e4, pext / n_gas, '--', label=f'test')
+                    ax1.semilogx(w * 1e4, pext / n_gas, label=f'U={U_rad:.1f} -- n$_{{\\rm H}}$={n_gas:.1e} -- f$_{{\\rm max}}$={f_max:.2f}')
                     if np.sum(abs(A_per_Ngas/A_per_Ngas.max()-A_per_Ngas_pre))<=1e-19:
                         ax11.loglog(w * 1e4, A_per_Ngas,color='k',ls='--')
                     else:
