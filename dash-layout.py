9587b721b22319ea104d67df19d7f1f07f4d8bee
lengoctram <91097424+lengoctram@users.noreply.github.com>
2024-08-29 00:32:37 +0200

    Update dash-layout.py

--- dash-layout.py
+++ dash-layout.py
@@ -106,15 +106,15 @@ if rat_theory == 'MRAT':
     Ncl = st.sidebar.slider('Specify Ncl', 10., 1.e5, 10., format='%.1e')
 
 ratd,Smax=('off',-1.e-99)
-c1,c2,c3=st.sidebar.columns(3)
-with c1:
-    ratd = c1.checkbox('RAT-D')
-    if (ratd):
-        ratd='on'
-        Smax=c2.selectbox('Smax',[1e5,1e6,1e7,1e8,1e9,1e10],format_func=lambda x: '{:.1e}'.format(x))
-        with c3.expander("Definition"):
-            st.write('''Maximum tensile strength of grain ($\\rm erg\\,cm^{-3}$) -- characterizing grain's porosity
-            ''')
+# c1,c2,c3=st.sidebar.columns(3)
+# with c1:
+#     ratd = c1.checkbox('RAT-D')
+#     if (ratd):
+#         ratd='on'
+#         Smax=c2.selectbox('Smax',[1e5,1e6,1e7,1e8,1e9,1e10],format_func=lambda x: '{:.1e}'.format(x))
+#         with c3.expander("Definition"):
+#             st.write('''Maximum tensile strength of grain ($\\rm erg\\,cm^{-3}$) -- characterizing grain's porosity
+#             ''')
 # Row A
 st.markdown('### Parameters')
 col1, col2, col3 = st.columns(3)
