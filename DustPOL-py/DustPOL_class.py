80138ad490c889003dfc9d5e19a084f8961b7d1c
lengoctram <91097424+lengoctram@users.noreply.github.com>
2024-08-28 11:29:24 +0200

    Add files via upload

---
+++ DustPOL-py/DustPOL_class.py
@@ -0,0 +1,270 @@
+##import Built-in functions
+import numpy as np
+import os,re
+import matplotlib.pyplot as plt
+import scipy.integrate as integrate
+import warnings
+from astropy import log, constants
+from matplotlib.colors import LogNorm
+from joblib import Parallel, delayed#, Memory
+from scipy.interpolate import interp1d
+from astropy.io import fits
+from scipy import interpolate
+
+##import customized functions
+from decorators import auto_refresh, printProgressBar, bcolors
+import rad_func
+import disrupt
+import qq
+import radiation
+import align 
+import size_distribution
+import pol_degree
+##Ignore the warnings
+warnings.filterwarnings('ignore')
+
+class DustPOL:
+    """This is the main routine of the DustPOL-py
+        -- update_radiation     : check and update for radiation
+        -- update_grain_size    : check and update for grain size
+        -- size_distribution    : calling size distribution
+        -- get_Planck_function  : calling Planck function
+        Inputs:
+        -------
+            These args are passed from the input file
+                + U,mean_lam,gamma, ##radiation
+                + Tgas,Tdust,ngas,  ##physical conditions
+                + ratd,Smax,        ##rotational disruption
+                + amin,amax,power_index,dust_type,dust_to_gas_ratio,GSD_law, ##grain-size
+                + RATalign,f_min,f_max,alpha,   ##alignment physics
+                + B_angle                       ##B-field inclination
+        Outputs:
+        --------
+            Some sub-routines to call-out
+                + cal_pol_abs   : to compute the degree of absorption polarisation
+                + cal_pol_emi   : to compute the degree of emisison polarisation
+                + starless_r0   : to compute the degree of absorption and emission polarisations
+                                :            of starless core at a given line of sight (r0 -- x-coorinate)
+                + starless      : to compute the degree of absorption and emission polarisations in starless core 
+
+        Examples:
+        ---------
+        - Starless core with the line of sight Av fixed for different amax
+                starless(Av_fixed_amax=True,fixed_amax_value=1.e-4)
+        - Starless core with the line of sight Av varried for different amax
+                starless(Av_fixed_amax=False)
+    """
+
+    @auto_refresh
+    def __init__(self):
+        from DustPOL_io import (
+            U, u_ISRF, mean_lam, gamma, Tgas, Tdust, ngas,
+            ratd, Smax, amin, amax, power_index, rho, dust_type, 
+            dust_to_gas_ratio, GSD_law, RATalign, f_max,alpha,
+            B_angle, Bfield, Ncl, phi_sp, fp, rflat, rout,nsample,
+            path, pc
+        )
+
+        self.U = U             #No-unit
+        self.u_ISRF=u_ISRF     #erg cm-3
+        self.gamma=gamma       #No-unit
+        self.mean_lam=mean_lam #cm
+        self.Tgas=Tgas         #K
+        self.Tdust=Tdust       #K
+        self.ngas=ngas         #cm-3
+        self.ratd=ratd         #[option]
+        self.Smax=Smax         #erg cm-3
+        self.amin=amin         #cm
+        self.amax=amax         #cm
+        self.power_index      =power_index       #No-unit
+        self.dust_type        =dust_type         #[option]
+        self.dust_to_gas_ratio=dust_to_gas_ratio #No-unit
+        self.GSD_law          =GSD_law           #[option]
+        self.RATalign         =RATalign          #[option]
+        self.Bfield = Bfield                     #[MRAT] -- otherwise, nan
+        self.Ncl    = Ncl                        #[MRAT] -- otherwise, nan
+        self.phi_sp = phi_sp                     #[MRAT] -- otherwise, nan
+        self.fp     = fp                         #[MRAT] -- otherwise, nan
+        self.f_min  = 0.0      #%
+        self.f_max  = f_max    #%
+        self.alpha  = alpha    #No-unit
+        self.B_angle=B_angle   #radiant
+        self.rho    =rho       #g cm-3
+        self.Urange_tempdist=[]
+
+        ##test starless core
+        # self.n0_gas=1e5
+        self.rflat = rflat #cm #17000.*constants.au.cgs.value
+        self.rout  = rout  #cm #624.e6*constants.au.cgs.value
+        self.nsample= nsample #int 5#50
+
+        # # ------- get constants -------
+        self.pc = pc
+
+        # # ------- get path to directory -------
+        self.path=path
+
+        # ------- Initialization wavelength, grain size, and cross-sections from the file -------
+        if self.dust_type=='astro' or self.dust_type=='Astro':
+            hdr_lines=4
+            skip_lines=4
+            len_a=169
+            len_w=1129
+            num_cols=8
+            self.Data_aAstro=rad_func.readDC(self.path+'data/astrodust/Q_aAstro_%.3f'%(self.alpha)+'_P0.2_Fe0.00.DAT',hdr_lines,skip_lines,len_a,len_w,num_cols)
+
+        else:
+            #BELOW DOESN'T WORK FOR OBLATE SHAPE WITH S=2
+            if float(self.alpha)==0.3333:
+                hdr_lines = 4
+                skip_lines=4
+                len_a_sil=70
+                len_a_car=100
+                len_w=800
+                num_cols=8
+            elif float(self.alpha)==2.0:
+                hdr_lines=4
+                skip_lines=4
+                len_a_sil=160
+                len_a_car=160
+                len_w=104
+                num_cols=8
+            else:
+                log.error('Values of alpha is not regconized! [\033[1;5;7;91m failed \033[0m]')
+            self.Data_sil = rad_func.readDC(self.path+'data/Q_aSil2001_'+str(self.alpha)+'_p20B.DAT',hdr_lines,skip_lines,len_a_sil,len_w,num_cols)
+            self.Data_mCBE = rad_func.readDC(self.path+'data/Q_amCBE_'+str(self.alpha)+'.DAT',hdr_lines,skip_lines,len_a_car,len_w,num_cols)
+        self.get_coefficients_files()
+        
+        # ------- Initialization grain-size distribution -------
+        self.grain_size_distribution()  
+
+    @auto_refresh
+    def update_radiation(self):
+        self.U = radiation.radiation_retrieve(self).retrieve()
+        # self.U = rad.retrieve()
+
+    @auto_refresh
+    def update_grain_size(self,a):
+        #update radiation
+        #self.update_radiation()
+
+        if self.amax>max(a):
+            raise ValueError('SORRY - amax should be %.5f [um] at most [\033[1;5;7;91m failed \033[0m]'%(max(a)*1e4))
+
+        self.lmin = np.searchsorted(a, self.amin)
+        self.lmax = np.searchsorted(a, min(self.amax, disrupt.radiative_disruption(self).a_disrupt(a))) if self.ratd == 'on' else np.searchsorted(a, self.amax + 0.1 * self.amax)
+
+        self.a = a[self.lmin:self.lmax]
+        self.na = len(self.a)
+        return
+
+    @auto_refresh
+    def dP_dT(self):
+        ##This function reads the dust temperature distribution, pre-calculated
+        qT = rad_func.T_dust(self.path, self.na, self.U)#T_dust(na,UINDEX)
+        T_gra = qT[0]
+        T_sil = qT[1]
+        dP_dlnT_gra = qT[2]
+        dP_dlnT_sil = qT[3]
+        return T_sil,T_gra,dP_dlnT_sil,dP_dlnT_gra
+
+    @auto_refresh
+    def get_coefficients_files(self):
+        ##This function reads the cross-sections, pre-calculated
+        ##The outputs are 2d-array: a function of wavelength and grain-size
+        if self.dust_type.lower()=='astro':
+            self.w = self.Data_aAstro[1,:,0]*1e-4 ## wavelength in cm
+            a = self.Data_aAstro[0,0,:]*1e-4      ## grain size in cm
+            self.update_grain_size(a)             ## update grain size --> self.a
+            [self.Qext_astro, self.Qabs_astro, self.Qpol_astro, self.Qpol_abs_astro] = qq.Qext_grain_astrodust(self.Data_aAstro,self.w,self.a,self.alpha)
+            return
+        else:
+            self.w = rad_func.wave(self.path)     ##good for prolate shape
+            a = rad_func.a_dust(self.path,10.0)[1] ##good for prolate shape
+            self.update_grain_size(a)    ##update grain size
+            
+            if float(self.alpha)==2.0: ##efficiences data from POLARIS has a problem <-- fixed
+                [self.Qext_sil, self.Qabs_sil, self.Qpol_sil, self.Qpol_abs_sil] = qq.Qext_grain(self.Data_sil,self.w,self.a,self.alpha,fixed=False,wmin=172e-4,wmax=628e-4,dtype='sil')        
+                [self.Qext_amCBE, self.Qabs_amCBE, self.Qpol_amCBE, self.Qpol_abs_amCBE] = qq.Qext_grain(self.Data_mCBE,self.w,self.a,self.alpha,fixed=True,wmin=172e-4,wmax=300e-4,dtype='car')
+            else:
+                [self.Qext_sil, self.Qabs_sil, self.Qpol_sil, self.Qpol_abs_sil] = qq.Qext_grain(self.Data_sil,self.w,self.a,self.alpha)        
+                [self.Qext_amCBE, self.Qabs_amCBE, self.Qpol_amCBE, self.Qpol_abs_amCBE] = qq.Qext_grain(self.Data_mCBE,self.w,self.a,self.alpha)
+            return
+
+    @auto_refresh
+    def grain_size_distribution(self,fix_amax=False,fix_amax_value=None):
+        ##This function compute the grain-size distribution
+        ##The output is a 1d-array: a function of grain-size
+        if self.dust_type.lower()=='astro':
+            if not fix_amax:
+                GSD_params = [self.a.min(),self.a.max(),2.74,self.dust_to_gas_ratio,self.power_index]
+            else:
+                GSD_params = [self.a.min(),fix_amax_value,2.74,self.dust_to_gas_ratio,self.power_index]
+            self.dn_da_astro = size_distribution.dnda_astro(self.a,sizedist=self.GSD_law,MRN_params=GSD_params)
+            return
+        else:
+            self.dn_da_gra = size_distribution.dnda(6,'carbon',self.a,self.GSD_law,self.power_index,self.dust_to_gas_ratio)
+            self.dn_da_sil = size_distribution.dnda(6,'silicate',self.a,self.GSD_law,self.power_index,self.dust_to_gas_ratio)
+            return
+
+    @auto_refresh
+    def get_Planck_function(self,Tdust,dP_dlnT=None):
+        ##This function calculates the Planck-function
+        ##The output is a 2d-array: a function of wavelength and grain-size
+        if dP_dlnT is None:
+            B_  = rad_func.planck_equi(self.w,self.na,Tdust) ##Tdust must be an array with 'na' element
+        else:
+            B_ = rad_func.planck(self.w,self.na,Tdust,dP_dlnT) ##function of U and na
+        return B_
+
+    @auto_refresh
+    def extinction(self):
+        ##This function return the extinction curve, normalized by Ngas
+        if self.dust_type.lower()=='astro':
+            dtau = self.Qext_astro * np.pi*self.a**2 * self.dn_da_astro
+        elif self.dust_type.lower()=='sil':# in ['sil','silicate']:
+            dtau = self.Qext_sil * np.pi*self.a**2 * self.dn_da_sil
+        else:
+            dtau_sil = self.Qext_sil * np.pi*self.a**2 * self.dn_da_sil
+            dtau_car = self.Qext_amCBE * np.pi*self.a**2 * self.dn_da_gra
+            dtau = dtau_sil + dtau_car
+        
+        tau_per_Ngas = integrate.simps(dtau,self.a)
+        return 1.086*tau_per_Ngas
+
+    @auto_refresh
+    def cal_pol_abs(self,progress=False):
+        ##This function calculates the degree of starlight polarization
+        ##The output is a 1d-array: a function of wavelength
+        self.progress=progress
+        if not (self.progress):
+            print('\t \033[1;7;34m U=%.3f \033[0m   \t\t '%(self.U))
+        w,dP_abs_sil,dP_abs_mix = pol_degree.pol_degree(self)._pol_degree_absorption_(self)
+        return w,dP_abs_sil,dP_abs_mix
+
+    @auto_refresh
+    def cal_pol_emi(self,tau=0.0,Tdust=None,progress=False):#,get_planck_option=True):
+        ##This function calculates the degree of thermal dust polarization
+        ##The output is a 1d-array: a function of wavelength
+        ## If dust_type is Astrodust (astro):
+        ##      return [total intensity, polarized intensity, zeros_array], [pol. degree, zeros_array]
+        ## If dust_type is silicate (sil):
+        ##      return [total intensity, polarized intensity of sil, polarized intensity of sil+car], [pol. degree of sil, pol. degree of sil+car]
+
+        self.progress=progress
+
+        if self.dust_type.lower()=='astro':
+            if Tdust is None:
+                Tdust = 16.4* self.U**(1./6) * (self.a/1.e-5)**(-1./15)#* np.ones(self.na)
+                log.info('\033[1;7;34m U=%.3f : radiation -->> Tdust \033[0m   \t\t '%(self.U))
+            elif isinstance(Tdust,(float,int)):
+                log.info('\033[1;7;34m U=%.3f and Tdust=%.3f (K) \033[0m   \t\t '%(self.U,Tdust))
+                Tdust = float(Tdust) * (self.a/1.e-5)**(-1./15)
+            self.B_astro=self.get_Planck_function(Tdust)
+        else:
+            T_sil,T_gra,dP_dlnT_sil,dP_dlnT_gra = self.dP_dT() ##function of U and na
+            self.B_gra=self.get_Planck_function(T_gra,dP_dlnT_gra)
+            self.B_sil=self.get_Planck_function(T_sil,dP_dlnT_sil)
+
+        w,I_list,P_list = pol_degree.pol_degree(self)._pol_degree_emission_(self,tau=tau)
+        return w,I_list,P_list
---
+++ DustPOL-py/DustPOL_io.py
@@ -0,0 +1,135 @@
+import numpy as np
+import os
+from astropy import log, constants
+# -------------------------------------------------------------------------
+# Physical constants
+# -------------------------------------------------------------------------
+au  = constants.au.cgs.value
+H   = 6.625e-27
+C   = 3e10
+K   = 1.38e-16
+amu = 1.6605402e-24 #atomic mass unit
+yr  = 365.*24.*60.*60.
+pc  = constants.pc.cgs.value
+
+#path= '/Users/lengoctram/Documents/postdoc/MPIfR/research/dustpol/src_v1.5/DustPOL-RAT+MRAT+2-layer_original/'#os.getcwd()
+path = os.getcwd()+'/DustPOL/'
+
+## -------------------------------------------------------------------------
+## Experiment setup
+##-------------------------------------------------------------------------
+# from optparse import OptionParser
+# parser = OptionParser()
+# parser.add_option("-f", "--file",
+#                   dest="file",
+#                   help="file with data")
+# (options, args) = parser.parse_args()
+
+# inputs = options.file                 
+
+inputs = path+'input.dustpol'
+q = np.genfromtxt(inputs,skip_header=1,dtype=None,names=['names','params'],\
+comments='!',usecols=(0,1),encoding='utf=8')
+params     = q['params']
+cloud      = params[0]
+ratd       = params[1]
+working_lam= eval(params[2])
+rin        = eval(params[3]) * au #cm
+rout       = eval(params[4]) * au #cm
+rflat      = eval(params[5]) * au #cm
+nsample    = eval(params[6])
+U          = eval(params[7])
+gamma      = eval(params[8])
+mean_lam   = eval(params[9])*1e-4 #cm
+dpc        = params[10]+'kpc'
+ngas       = eval(params[11])
+Tgas       = eval(params[12])
+Avrange    = eval(params[13])
+mgas       = 1.3*amu #90%H + 10%He
+dust_type  = params[14]
+amin       = eval(params[15])*1e-4 #cm
+amax       = eval(params[16])*1e-4 #cm
+Tdust      = eval(params[17])
+rho        = eval(params[18])
+alpha      = eval(params[19])
+Smax       = eval(params[20])
+dust_to_gas_ratio=eval(params[21])
+GSD_law    = params[22]
+power_index= eval(params[23])
+RATalign = params[24].lower() # RAT or MRAT
+f_max    = eval(params[25])
+B_angle  = eval(params[27])*np.pi/180 # rad.
+if RATalign=='mrat':
+    Bfield = eval(params[26])
+    Ncl    = eval(params[28])
+    phi_sp = eval(params[29])
+    fp     = eval(params[30])
+else:
+    Bfield = np.nan
+    Ncl    = np.nan
+    phi_sp = np.nan
+    fp     = np.nan
+# model_layer = eval(params[31])
+
+# parallel   = eval(params[35])
+# n_jobs     = eval(params[36])
+# overwrite  = params[37]
+# if overwrite=='No' or overwrite=='no':
+#     checking=True
+# else:
+#     checking=False
+u_ISRF = 8.64e-13 # typical interstellar radiation field
+
+class output():
+    def __init__(self,parent,filename,data):
+        self.U=parent.U
+        self.alpha=parent.alpha
+        # subpath = path+'output/starless/astrodust/'#U=%.2f'%(self.U)+'/'
+        # if not os.path.exists(subpath):
+        #     os.mkdir(subpath)
+        # subsubpath = subpath+'U=%.2f_alpha=%.4f'%(self.U,self.alpha)+'/Av_fixed_amax/'
+        # if not os.path.exists(subsubpath):
+        #     os.mkdir(subsubpath)
+
+        subpath = path+'output/starless/astrodust/'+'U=%.2f_alpha=%.4f'%(self.U,self.alpha)+'/Av_fixed_amax/'
+        if not os.path.exists(subpath):
+            os.makedirs(subpath)
+        self.filename=subpath+filename
+        self.ngas=parent.ngas
+        self.mean_lam=parent.mean_lam
+        self.gamma=parent.gamma
+        self.Av_array=parent.Av_array
+        self.data=data
+
+        # output_abs = path+'amax=%.2f'%(self.amax*1e4)+'_abs.dat'
+        # output_emi = path+'amax=%.2f'%(self.amax*1e4)+'_emi.dat'
+
+        self.file_save()
+
+    def file_save(self):
+        f=open(self.filename,'w')
+        f.write('U=%.3f \n'%self.U)
+        f.write('ngas=%.3e (cm-3) \n'%self.ngas)
+        f.write('mean_lam=%.3f (um) \n'%(self.mean_lam*1e4))
+        f.write('gamma=%.3f \n'%self.gamma)
+        f.write('! \n')
+        f.write('Av= ')
+        f.write(",".join(str("{:.3f}".format(iAv)) for iAv in self.Av_array) + "\n")
+        f.write('! \n')
+        #keys=sorted(data_save.keys())
+        keys=list(self.data.keys())
+        print('   '.join(keys), end="\n",file=f)
+        for i in range(len(self.data[keys[0]])):
+            line=''
+            for k in keys:
+                # line=line+str(self.eformat(self.data[k][i],4,2))+'\t '
+                line=line+str("{:.3e}".format(self.data[k][i]))+'\t '
+
+            print(line,end="\n",file=f)
+        f.close()
+
+    def eformat(self,f, prec, exp_digits):
+        s = "%.*e"%(prec, f)
+        mantissa, exp = s.split('e')
+        # add 1 to digits as 1 is taken by sign +/-
+        return "%se%+0*d"%(mantissa, exp_digits+1, int(exp))
