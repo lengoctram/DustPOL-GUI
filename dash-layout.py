4af773549c7a8b325d94d5a447263365e8d49379
lengoctram <91097424+lengoctram@users.noreply.github.com>
2024-08-28 13:48:37 +0200

    Update dash-layout.py

--- dash-layout.py
+++ dash-layout.py
@@ -147,7 +147,7 @@ def plot_figures():
                     w, pext, pem, A_per_Ngas = results
                     w_new = np.linspace(w[0],w[-1],5000)
                     ## smooth pext
-                    pext_new=scipy.interpolate.interp1d(w,pext)(w_new)
+                    pext_new=scipy.interpolate.interp1d(w,pext,kind='cubic')(w_new)
                     ax1.semilogx(w_new * 1e4, pext_new / n_gas, label=f'U={U_rad:.1f} -- n$_{{\\rm H}}$={n_gas:.1e} -- f$_{{\\rm max}}$={f_max:.2f}')
                     ax1.semilogx(w * 1e4, pext / n_gas, '--', label=f'test')
                     if np.sum(abs(A_per_Ngas/A_per_Ngas.max()-A_per_Ngas_pre))<=1e-19:
