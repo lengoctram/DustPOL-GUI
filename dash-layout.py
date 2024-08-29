747b556d2fa573564ea62c2dcc4b1d30cf51b1ca
lengoctram <91097424+lengoctram@users.noreply.github.com>
2024-08-29 14:10:37 +0200

    Update dash-layout.py

--- dash-layout.py
+++ dash-layout.py
@@ -121,7 +121,7 @@ button = st.sidebar.button("Clear All Caches")
 if button:   
     st.cache_data.clear()
 with st.sidebar.expander("explanation"):
-    st.write('To clear all memory caches. \\ 
+    st.write('To clear all memory caches. 
     Caches are on your disk and it is recommended to clear them all after a while!')
 
 # Row A
